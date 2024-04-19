873658bb07272a747bbfa29aba22f7f339c725fd
Alexander Mandrikov <mandrikov@pm.me>
2024-04-19 19:40:33 +0700

    init gradle project

---
+++ app/build.gradle.kts
@@ -0,0 +1,43 @@
+group = "hexlet.code"
+version = "0.0.1"
+
+plugins {
+    // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
+    // alias(libs.plugins.jvm)
+    kotlin("jvm") version "1.9.23"
+
+    // Apply the application plugin to add support for building a CLI application in Java.
+    application
+}
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // This dependency is used by the application.
+    // implementation(libs.guava)
+}
+
+// testing {
+//     suites {
+//         // Configure the built-in test suite
+//         val test by getting(JvmTestSuite::class) {
+//             // Use Kotlin Test test framework
+//             useKotlinTest("1.9.23")
+//         }
+//     }
+// }
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass = "$group.App"
+}
---
+++ app/src/main/kotlin/hexlet/code/App.kt
@@ -0,0 +1,10 @@
+package hexlet.code
+
+class App {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            println("Hello World!")
+        }
+    }
+}
